2b38b314385c129851a0cf94cda2c8c8e13f813e
Oksana Tkachenko <oksana@crystax.net>
2018-05-07 12:25:34 +0300

    changed path for server.js and adds to server index.html

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-//app.use(express.static(__dirname));
-app.get('/', (req, res) => {
-  res.status(200).send('Hello world!');
-});
+app.use(express.static(__dirname + '/public'));
 
 const PORT = process.env.PORT || 4000;
 
