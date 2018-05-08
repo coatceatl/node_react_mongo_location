bc3eb31fe4b0e32b9fca4f47ae774a3900ecaa83
Oksana Tkachenko <oksana@crystax.net>
2018-05-08 14:21:07 +0300

    adds react-router-dom and refactored component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h2>Vegans Location</h2>
+        <BrowserRouter>
+          <div>
+            <Route path='/' component={Header} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
