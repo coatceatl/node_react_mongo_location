271c9b87919313302af47aa9bb443f655cfc111d
Oksana Tkachenko <oksana@crystax.net>
2018-05-08 13:59:28 +0300

    adds component App.jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Hi from React</h2>
-      </div>
-    );
-  }
-}
+import App from './App.jsx';
 
 ReactDOM.render(
   <App />,
