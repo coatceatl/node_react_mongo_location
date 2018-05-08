9566170583b427b13b14de66b11ce2ca9b822186
Oksana Tkachenko <oksana@crystax.net>
2018-05-09 00:14:11 +0300

    adds google map

--- src/components/SearchMap.jsx
+++ src/components/SearchMap.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import {
+  withScriptjs,
+  withGoogleMap,
   GoogleMap,
   Marker,
 } from 'react-google-maps';
+import keys from '../../config/keys';
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={10}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
 
 class SearchMap extends Component {
   render() {
     return(
-      <h1>Google Map</h1>
+      <MyMapComponent
+        isMarkerShown
+        googleMapURL='https://maps.googleapis.com/maps/api/js?keys.mapKey&v=3.exp&libraries=geometry,drawing,places'
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     );
   }
 }
