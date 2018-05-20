4d1be7e8ada85c8bcfb23e058e0dd19ebd34ce91
Oksana Tkachenko <oksana@crystax.net>
2018-05-20 20:46:36 +0300

    added access and refresh token, steel need to add new user in database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 
 const app = express();
@@ -13,8 +13,10 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-      console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile:', profile);
     }
   )
 );
@@ -24,6 +26,11 @@ app.get(
   passport.authenticate('google', {
     scope: ['profile', 'email']
   })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
 );
 
 const PORT = process.env.PORT || 4000;
