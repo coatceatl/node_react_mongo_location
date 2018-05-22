941eb91d8f32efa93ed908d9667e20df1c3dcb2b
Oksana Tkachenko <oksana@crystax.net>
2018-05-22 12:09:21 +0300

    refactored promises with async/await

--- services/passport.js
+++ services/passport.js
@@ -21,18 +21,15 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            console.log(existingUser);
-            done(null, existingUser);
-          } else {
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        })
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id })
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
+      }
     }
   )
 );
