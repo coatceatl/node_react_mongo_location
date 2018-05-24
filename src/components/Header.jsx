3dfd45aba04b0eb99691e5dc8e2989a3f0dd4799
Oksana Tkachenko <oksana@crystax.net>
2018-05-24 21:51:05 +0300

    added states in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <a href='/auth/google'>Sign In</a>;
+      default:
+        return <a href='#'>Logout</a>;
+    }
+  }
+
   render() {
     return (
       <div className='header-wrapper'>
         <a className='header-logo' href='#'>Logo</a>
         <ul className='header-menu'>
-          <li>
-            <a href='/auth/google'>Sign In</a>
-          </li>
+          { this.renderContent() }
         </ul>
       </div>
     );
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header)
