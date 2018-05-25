9182db844178c3ddfe20dd73df3b871b75a9df7d
Oksana Tkachenko <oksana@crystax.net>
2018-05-25 15:48:36 +0300

    changed style for component navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <a className="navbar-brand" href="#">Logo</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -17,11 +17,10 @@ class Navigation extends Component {
             <li className="nav-item">
               <a className="nav-link" href="#">Link</a>
             </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
     );
