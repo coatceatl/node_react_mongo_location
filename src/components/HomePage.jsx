ec942f44bcfaa73a230ecbaf29010a294f061083
Oksana Tkachenko <oksana@crystax.net>
2018-05-26 18:55:59 +0300

    changed component HomePage(added jumbotron and search)

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,23 @@ import React, { Component } from 'react';
 class HomePage extends Component {
   render() {
     return (
-      <h1>Welcome to Home Page!</h1>
+      <div className='jumbotron container'>
+        <h1 className='display-4'>Discover the perfect place!</h1>
+        <form className="form-inline my-2 my-lg-0">
+          <div className="form-group">
+            <label for="menuItem"></label>
+            <select className="form-control" id="menuItem">
+              <option>Find...</option>
+              <option>Top</option>
+              <option>Cofee</option>
+              <option>Breacfast</option>
+              <option>Restaurant</option>
+            </select>
+          </div>
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
     );
   }
 }
